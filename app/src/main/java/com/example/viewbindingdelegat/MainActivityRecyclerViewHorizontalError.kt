9bbadbe0487b0416a489f738c3aef7edbf52f479
Denis <balaev@mail.ru>
2023-09-02 00:48:31 +0300

    Решение через post

--- app/src/main/java/com/example/viewbindingdelegat/MainActivityRecyclerViewHorizontalError.kt
+++ app/src/main/java/com/example/viewbindingdelegat/MainActivityRecyclerViewHorizontalError.kt
@@ -18,20 +18,39 @@ class MainActivityRecyclerViewHorizontalError : AppCompatActivity(R.layout.activ
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+
         val dataList = fillList().toMutableList()
         val layoutManagers = LinearLayoutManager(this@MainActivityRecyclerViewHorizontalError,LinearLayoutManager.HORIZONTAL,false)
         val adapters = CustomAdapter(dataList)
+
+        val addListener = object : View.OnLayoutChangeListener{
+            override fun onLayoutChange(v: View?, left: Int, top: Int, right: Int, bottom: Int, oldLeft: Int, oldTop: Int, oldRight: Int, oldBottom: Int) {
+                val lastVisibleItem: Int = layoutManagers.findLastCompletelyVisibleItemPosition()
+                if (lastVisibleItem > 0) {
+                    Log.d("TEST", "LastItem: $lastVisibleItem")
+                    adapters.visibily = lastVisibleItem + 1
+                    dataList[lastVisibleItem] = (dataList.size - lastVisibleItem).toString()
+                    binding.rv.removeOnLayoutChangeListener(this);
+                    binding.rv.post {binding.rv.adapter?.notifyDataSetChanged()}
+                }
+            }
+
+        }
+
         binding.rv.apply {
             layoutManager = layoutManagers
             adapter = adapters
-            post {
-                val lastVisibleItem =layoutManagers.findLastCompletelyVisibleItemPosition()
-                Log.d("TEST", "LastItem: $lastVisibleItem")
-                adapters.visibily = lastVisibleItem + 1
-                dataList[lastVisibleItem] = (dataList.size - lastVisibleItem).toString()
-                this.adapter?.notifyDataSetChanged()
-            }
+            addOnLayoutChangeListener(addListener)
         }
+
+
+        /*binding.rv.post {
+            val lastVisibleItem =layoutManagers.findLastCompletelyVisibleItemPosition()
+            Log.d("TEST", "LastItem: $lastVisibleItem")
+            adapters.visibily = lastVisibleItem + 1
+            dataList[lastVisibleItem] = (dataList.size - lastVisibleItem).toString()
+            this.binding.rv.adapter?.notifyDataSetChanged()
+        }*/
     }
 
     private fun fillList(): List<String> {
